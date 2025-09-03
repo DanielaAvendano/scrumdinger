e1d9dfae3c3341d0108e0c6c057bbc8b5cfba7e3
DanielaAvendano <danialejandra17@gmail.com>
2025-09-03 10:42:28 -0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Daniela Avendaño on 3/09/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
