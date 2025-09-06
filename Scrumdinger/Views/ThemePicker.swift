28eaeab573a83cbf1e41d74be9d0b04282729f58
DanielaAvendano <danialejandra17@gmail.com>
2025-09-06 18:12:06 -0500

    @state and @binding

---
+++ Scrumdinger/Views/ThemePicker.swift
@@ -0,0 +1,8 @@
+//
+//  ThemePicker.swift
+//  Scrumdinger
+//
+//  Created by Daniela Avendaño on 5/09/25.
+//
+
+import Foundation
---
+++ Scrumdinger/Views/ThemeView.swift
@@ -0,0 +1,8 @@
+//
+//  ThemeView.swift
+//  Scrumdinger
+//
+//  Created by Daniela Avendaño on 5/09/25.
+//
+
+import Foundation
